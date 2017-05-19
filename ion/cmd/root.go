4591f5fe8846c48e70418f0346652415fcf286b8
Julien Kauffmann <julien.kauffmann@freelan.org>
2017-05-19 19:45:38 -0400

    Better behavior on error

--- ion/cmd/root.go
+++ ion/cmd/root.go
@@ -54,13 +54,13 @@ Type "ion -h" to discover all the other available commands.`,
 			powerLineModem.Close()
 		}
 	},
+	SilenceUsage: true,
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
-		fmt.Println(err)
 		os.Exit(1)
 	}
 }
